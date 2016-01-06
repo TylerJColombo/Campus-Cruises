26b1be33ba0f5adf8e58cf9399d8c1035b6d0ee6
Trevor Hodde <trhodde@wpi.edu>
2016-01-06 13:28:13 -0500

    Adding a new projct

---
+++ Campus-Cruises/Campus-Cruises/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Campus-Cruises
+//
+//  Created by Trevor Hodde on 1/6/16.
+//  Copyright © 2016 Trevor Hodde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
