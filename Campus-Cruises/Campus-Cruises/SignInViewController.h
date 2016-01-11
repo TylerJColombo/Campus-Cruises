a9044ec22390018a22cfaa5750fd3360d9e8c7f0
Trevor Hodde <trhodde@wpi.edu>
2016-01-11 18:20:25 -0500

    Users can login now as well

---
+++ Campus-Cruises/Campus-Cruises/SignInViewController.h
@@ -0,0 +1,14 @@
+//
+//  SignInViewController.h
+//  Campus-Cruises
+//
+//  Created by Trevor Hodde on 1/11/16.
+//  Copyright © 2016 Trevor Hodde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface SignInViewController : UIViewController
+
+@end
---
+++ Campus-Cruises/Campus-Cruises/SignInViewController.m
@@ -0,0 +1,49 @@
+//
+//  SignInViewController.m
+//  Campus-Cruises
+//
+//  Created by Trevor Hodde on 1/11/16.
+//  Copyright © 2016 Trevor Hodde. All rights reserved.
+//
+
+#import "SignInViewController.h"
+
+@interface SignInViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@end
+
+@implementation SignInViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+// TODO -- add error checking:
+//      1) Check if username exists
+//      2) Check if password is correct
+//      3) Remove back button to avoid weird double login issues
+- (IBAction)signIn:(UIButton *)sender {
+    //Login button pressed
+    [PFUser logInWithUsernameInBackground:self.usernameTextField.text password:self.passwordTextField.text block:^(PFUser *user, NSError *error) {
+        if (user) {
+            //Open the wall
+            [self performSegueWithIdentifier:@"LoginSuccesful" sender:self];
+        } else {
+            //Something bad has ocurred
+            NSString *errorString = [[error userInfo] objectForKey:@"error"];
+            //UIAlertView *errorAlertView = [[UIAlertView alloc] initWithTitle:@"Error" message:errorString delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+            //[errorAlertView show];
+        }
+    }];
+}
+
+
+@end
